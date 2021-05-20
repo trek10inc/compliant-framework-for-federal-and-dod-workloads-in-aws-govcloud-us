1fba036c7b4dbc84c1648c10193374e7301f4c43
Justin Courtright <jcourtright@trek10.com>
2021-05-20 16:53:00 -0400

    updates

--- deployment/build-and-deploy-s3-dist.sh
+++ deployment/build-and-deploy-s3-dist.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+echo "cleaning"
+rm -rf ./global-s3-assets
+rm -rf ./regional-s3-assets
+
+echo "clean"
+
 ## Parameter definition block
-DIST_OUTPUT_BUCKET="super-leaky-dod-secrets-bucket"
-SOLUTION_NAME="compliant-framework-for-federal-and-dod-workloads-in-aws-govcloud-us"
-VERSION="1.1.2"
-ACCOUNT_ALIAS="dod-dla-te-test-com-central-kernel"
+export STACK_NAME="trek10-dla-deployment"
+export DIST_OUTPUT_BUCKET="dod-framework-testing"
+export REGION="us-east-1"
+export SOLUTION_NAME="compliant-framework-for-federal-and-dod-workloads-in-aws-govcloud-us"
+export VERSION="1.1.4"
+export ACCOUNT_ALIAS="dod-dla-te-test-com-central-kernel"
+export STACK_NAME="trek10-dla-deployment"
 
 echo "Awsuming into role..."
 awsume $ACCOUNT_ALIAS
@@ -20,8 +29,8 @@ chmod +x ./build-s3-dist.sh
 ./build-s3-dist.sh $DIST_OUTPUT_BUCKET $SOLUTION_NAME $VERSION
 
 echo "Uploading to S3..."
-aws s3 cp ./global-s3-assets/ s3://$DIST_OUTPUT_BUCKET/$SOLUTION_NAME/$VERSION/ --recursive --acl bucket-owner-full-control
-aws s3 cp ./regional-s3-assets/ s3://$DIST_OUTPUT_BUCKET/$SOLUTION_NAME/$VERSION/ --recursive --acl bucket-owner-full-control
+aws s3 cp ./global-s3-assets/ s3://$DIST_OUTPUT_BUCKET-$REGION/$SOLUTION_NAME/$VERSION/ --recursive --acl bucket-owner-full-control
+aws s3 cp ./regional-s3-assets/ s3://$DIST_OUTPUT_BUCKET-$REGION/$SOLUTION_NAME/$VERSION/ --recursive --acl bucket-owner-full-control
 
 
 echo "Finished Uploading"
@@ -33,5 +42,6 @@ aws cloudformation deploy --template ./regional-s3-assets/compliant-framework-go
   managementServicesAccountEmail=dla-aws-aliases+test-management-services-account@trek10.com \
   environmentNotificationEmail=dla-aws-aliases+test-env-notifications@trek10.com \
   coreNotificationEmail=dla-aws-aliases+test-core-notifications@trek10.com \
-  transitAccountEmail=dla-aws-aliases+test-transit-account@trek10.com
+  transitAccountEmail=dla-aws-aliases+test-transit-account@trek10.com \
+  --stack-name=$STACK_NAME
 echo "Finished deploying cloudformation template"
