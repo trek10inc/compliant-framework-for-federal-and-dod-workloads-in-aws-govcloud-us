29c3069cb1e574010b1dacbf73148d488e47c91e
Justin Courtright <jcourtright@trek10.com>
2021-05-20 13:57:33 -0400

    Finish build deploy script

--- Makefile
+++ Makefile
@@ -1,9 +1,12 @@
 build-and-deploy:
-	./deployment/build-and-deploy-s3-dist.sh
+	cd deployment
+	./build-and-deploy-s3-dist.sh
 
 build:
 	./deployment/build-s3-dist.sh
-
+verify:
+	cfn-lint source/repositories/**/*.yaml
+	cfn-lint source/lib/account-vending-machine/templates/*.yml --debug
 clean:
 	rm -rf deployment/global-s3-assets
 	rm -rf deployment/regional-s3-assets
--- deployment/build-and-deploy-s3-dist.sh
+++ deployment/build-and-deploy-s3-dist.sh
@@ -2,8 +2,11 @@
 ## Parameter definition block
 DIST_OUTPUT_BUCKET="super-leaky-dod-secrets-bucket"
 SOLUTION_NAME="compliant-framework-for-federal-and-dod-workloads-in-aws-govcloud-us"
-VERSION="1.1.0"
+VERSION="1.1.2"
+ACCOUNT_ALIAS="dod-dla-te-test-com-central-kernel"
 
+echo "Awsuming into role..."
+awsume $ACCOUNT_ALIAS
 
 cd ../source || echo "Invalid path"
 echo "Changing permission on test file"
@@ -22,3 +25,13 @@ aws s3 cp ./regional-s3-assets/ s3://$DIST_OUTPUT_BUCKET/$SOLUTION_NAME/$VERSION
 
 
 echo "Finished Uploading"
+
+echo "deploying using cloudformation"
+aws cloudformation deploy --template ./regional-s3-assets/compliant-framework-govcloud-account-product-v1.0.0.yml \
+  --parameter-overrides frameworkNotificationEmail=dla-aws-aliases+test-deploy-notifications@trek10.com \
+  loggingAccountEmail=dla-aws-aliases+test-logging-account@trek10.com \
+  managementServicesAccountEmail=dla-aws-aliases+test-management-services-account@trek10.com \
+  environmentNotificationEmail=dla-aws-aliases+test-env-notifications@trek10.com \
+  coreNotificationEmail=dla-aws-aliases+test-core-notifications@trek10.com \
+  transitAccountEmail=dla-aws-aliases+test-transit-account@trek10.com
+echo "Finished deploying cloudformation template"
